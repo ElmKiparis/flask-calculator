444e1f55b297503b2716173f0485507dc3876033
Олександр <elmkiparis@gmail.com>
2025-01-06 13:31:49 +0200

    Renamed resources 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ variable "REPOSITORY_URI" {
   type = string
 }
 
-resource "aws_lightsail_container_service" "flask_calculator_2" {
+resource "aws_lightsail_container_service" "flask_calculator_3" {
   name  = "flask-calculator"
   power = "nano"
   scale = 1
@@ -18,7 +18,7 @@ resource "aws_lightsail_container_service" "flask_calculator_2" {
   }
 }
 
-resource "aws_lightsail_container_service_deployment_version" "flask_calculator_deployment_2" {
+resource "aws_lightsail_container_service_deployment_version" "flask_calculator_deployment_3" {
   container {
     container_name = "flask-calculator"
     image          = "${var.REPOSITORY_URI}:latest"
@@ -41,6 +41,6 @@ resource "aws_lightsail_container_service_deployment_version" "flask_calculator_
     }
   }
   
-  service_name = aws_lightsail_container_service.flask_calculator.name
+  service_name = aws_lightsail_container_service.flask_calculator_3.name
   
 }
